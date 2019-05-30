638b8dc2654e81ef4db5f7c82df327f827e94200
yazidmehenni <yazid129@gmail.com>
2019-05-29 23:47:20 -0400

    add css grid watch layout

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -6,10 +6,28 @@ import RelatedVideos from '../../components/RelatedVideos/RelatedVideos';
 export default class Watch extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <RelatedVideos />
-        <Video id="-7fuHEEmEjs" />
-      </React.Fragment>
+      <div className="watch-grid">
+        <Video className="video" id="-7fuHEEmEjs" />
+        <div
+          className="metadata"
+          style={{ width: '100%', height: '100px', background: '#F91112' }}
+        >
+          Metadata
+        </div>
+        <div
+          className="video-info-box"
+          style={{ width: '100%', height: '100px', background: '#BD10E0' }}
+        >
+          Video Info box
+        </div>
+        <div
+          className="comments"
+          style={{ width: '100%', height: '100px', background: '#9013FE' }}
+        >
+          comments
+        </div>
+        <RelatedVideos className="relatedVideos" />
+      </div>
     );
   }
 }
