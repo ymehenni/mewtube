019248f23b32e9fe02978a6969d7ab2208485921
yazidmehenni <yazid129@gmail.com>
2019-05-30 10:11:56 -0400

    add video metadata

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -2,18 +2,14 @@ import React from 'react';
 import './Watch.scss';
 import Video from '../../components/Video/Video';
 import RelatedVideos from '../../components/RelatedVideos/RelatedVideos';
+import VideoMetadata from '../../components/VideoMetadata/VideoMetadata';
 
 export default class Watch extends React.Component {
   render() {
     return (
       <div className="watch-grid">
         <Video className="video" id="-7fuHEEmEjs" />
-        <div
-          className="metadata"
-          style={{ width: '100%', height: '100px', background: '#F91112' }}
-        >
-          Metadata
-        </div>
+        <VideoMetadata className="metadata" viewCount={1000} />
         <div
           className="video-info-box"
           style={{ width: '100%', height: '100px', background: '#BD10E0' }}
