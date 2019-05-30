b7edb29ed15c5086c6b7a3eef9069ba840f0a453
yazidmehenni <yazid129@gmail.com>
2019-05-29 22:35:59 -0400

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Home from './containers/Home/Home';
 import AppLayout from './components/AppLayout/AppLayout';
+import Watch from './containers/Watch/Watch';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
-        <Home />
+        <Switch>
+          <Route path="/watch" component={Watch} />
+          <Route path="/" component={Home} />
+        </Switch>
       </AppLayout>
     );
   }
---
+++ src/containers/Watch/Watch.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Watch.scss';
+
+export default class Watch extends React.Component {
+  render() {
+    return <div>Watch component goes here</div>;
+  }
+}
